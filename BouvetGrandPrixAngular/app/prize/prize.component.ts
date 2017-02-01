1f419f6bb32dbb9837ac88e3780d51540fdb2ed6
Kristian Løken Wille <kristian.wille@bouvet.no>
2017-02-01 19:38:17 +0100

    Added prize page

---
+++ BouvetGrandPrixAngular/app/prize/prize.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'about-screen',
+    templateUrl: 'app/prize/prize.component.html',
+    styleUrls: [
+        'app/prize/prize.component.css',
+    ]
+})
+
+export class PrizeComponent {
+
+}
