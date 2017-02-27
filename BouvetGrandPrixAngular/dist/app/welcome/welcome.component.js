4cde0dc6d518fa36b4ba85582c17b9fc06ca03de
Kristian Løken Wille <kristian.wille@bouvet.no>
2017-02-27 17:50:06 +0100

    Small bug fix

--- BouvetGrandPrixAngular/dist/app/welcome/welcome.component.js
+++ BouvetGrandPrixAngular/dist/app/welcome/welcome.component.js
@@ -1,2 +1,62 @@
-"use strict";var __decorate=this&&this.__decorate||function(e,t,o,c){var s,r=arguments.length,i=r<3?t:null===c?c=Object.getOwnPropertyDescriptor(t,o):c;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)i=Reflect.decorate(e,t,o,c);else for(var l=e.length-1;l>=0;l--)(s=e[l])&&(i=(r<3?s(i):r>3?s(t,o,i):s(t,o))||i);return r>3&&i&&Object.defineProperty(t,o,i),i},__metadata=this&&this.__metadata||function(e,t){if("object"==typeof Reflect&&"function"==typeof Reflect.metadata)return Reflect.metadata(e,t)},core_1=require("@angular/core"),toplist_service_1=require("../services/toplist.service"),WelcomeComponent=function(){function e(e,t){this.elementRef=e,this.toplistService=t,this.carouselSelector=0}return e.prototype.ngAfterViewInit=function(){this.getToplist(),$("#welcome_carousel").carousel();var e=$("#carousel_indicator_btn_active_0"),t=$("#carousel_indicator_btn_active_1");e.css("display","block"),t.css("display","none"),$("#welcome_carousel").on("slide.bs.carousel",function(){var e=$("#carousel_indicator_btn_active_0"),t=$("#carousel_indicator_btn_active_1");1===this.carouselSelector?(this.carouselSelector=0,e.css("display","block"),t.css("display","none")):(this.carouselSelector=1,e.css("display","none"),t.css("display","block"))})},e.prototype.getToplist=function(){var e=this;this.toplistService.getToplist().subscribe(function(t){return e.toplist=t},function(e){console.log(e)})},e}();WelcomeComponent=__decorate([core_1.Component({selector:"welcome-screen",templateUrl:"app/welcome/welcome.component.html",styleUrls:["app/welcome/welcome.component.css"]}),__metadata("design:paramtypes",[core_1.ElementRef,toplist_service_1.ToplistService])],WelcomeComponent),exports.WelcomeComponent=WelcomeComponent;
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var toplist_service_1 = require("../services/toplist.service");
+var WelcomeComponent = (function () {
+    function WelcomeComponent(elementRef, toplistService) {
+        this.elementRef = elementRef;
+        this.toplistService = toplistService;
+        this.carouselSelector = 0;
+    }
+    WelcomeComponent.prototype.ngAfterViewInit = function () {
+        this.getToplist();
+        $("#welcome_carousel").carousel();
+        var elem1 = $("#carousel_indicator_btn_active_0");
+        var elem2 = $("#carousel_indicator_btn_active_1");
+        elem1.css("display", "block");
+        elem2.css("display", "none");
+        $("#welcome_carousel").on('slide.bs.carousel', function () {
+            var elem1 = $("#carousel_indicator_btn_active_0");
+            var elem2 = $("#carousel_indicator_btn_active_1");
+            if (this.carouselSelector === 1) {
+                this.carouselSelector = 0;
+                elem1.css("display", "block");
+                elem2.css("display", "none");
+            }
+            else {
+                this.carouselSelector = 1;
+                elem1.css("display", "none");
+                elem2.css("display", "block");
+            }
+        });
+    };
+    WelcomeComponent.prototype.getToplist = function () {
+        var _this = this;
+        this.toplistService.getToplist()
+            .subscribe(function (toplist) { return _this.toplist = toplist; }, function (err) {
+            console.log(err);
+        });
+    };
+    return WelcomeComponent;
+}());
+WelcomeComponent = __decorate([
+    core_1.Component({
+        selector: 'welcome-screen',
+        templateUrl: 'app/welcome/welcome.component.html',
+        styleUrls: [
+            'app/welcome/welcome.component.css',
+        ]
+    }),
+    __metadata("design:paramtypes", [core_1.ElementRef,
+        toplist_service_1.ToplistService])
+], WelcomeComponent);
+exports.WelcomeComponent = WelcomeComponent;
 //# sourceMappingURL=welcome.component.js.map
